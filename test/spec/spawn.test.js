82d6b07905513dd3b4a211e05097038d54b4ac27
Kevin Malakoff <kmalakoff@gmail.com>
2021-07-25 14:15:17 -0700

    Put back versions

--- test/spec/spawn.test.js
+++ test/spec/spawn.test.js
@@ -18,9 +18,7 @@ var OPTIONS = {
   installedDirectory: path.join(TMP_DIR, 'installed'),
 };
 
-// TODO: put back
-// var VERSIONS = ['v14.1.0', 'v12.18.1', 'v0.8.25'];
-var VERSIONS = ['v14.1.0'];
+var VERSIONS = ['v14.1.0', 'v12.18.1', 'v0.8.25'];
 
 function addTests(version) {
   var INSTALL_DIR = path.resolve(path.join(OPTIONS.installedDirectory, version));
