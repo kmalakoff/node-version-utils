a2617fc421aa80611c420312f552af994122495d
Kevin Malakoff <kmalakoff@gmail.com>
2024-11-29 12:33:06 -0800

    Migrate to Biome and switch to TypeScript-based build process

---
+++ src/index.mjs
@@ -0,0 +1,5 @@
+import './polyfills.cjs';
+
+export { default as spawn } from './spawn';
+export { default as spawnSync } from './spawnSync';
+export { default as spawnOptions } from './spawnOptions';
