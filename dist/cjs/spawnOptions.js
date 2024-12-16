f6472222aa288a6e6fdb6534ade8f961025c89fc
Kevin Malakoff <kmalakoff@gmail.com>
2024-12-16 04:44:50 -0800

    Upgrade modules

--- dist/cjs/spawnOptions.js
+++ dist/cjs/spawnOptions.js
@@ -85,4 +85,4 @@ module.exports = function spawnOptions(installPath, options) {
         env: env
     });
 };
-/* CJS INTEROP */ if (exports.__esModule && exports.default) { Object.defineProperty(exports.default, '__esModule', { value: true }); for (var key in exports) exports.default[key] = exports[key]; module.exports = exports.default; }
+/* CJS INTEROP */ if (exports.__esModule && exports.default) { try { Object.defineProperty(exports.default, '__esModule', { value: true }); for (var key in exports) { exports.default[key] = exports[key]; } } catch (_) {}; module.exports = exports.default; }
--- dist/cjs/spawnSync.js
+++ dist/cjs/spawnSync.js
@@ -4,4 +4,4 @@ var spawnOptions = require('./spawnOptions');
 module.exports = function spawnSync(installPath, command, args, options) {
     return crossSpawn.sync(command, args, spawnOptions(installPath, options));
 };
-/* CJS INTEROP */ if (exports.__esModule && exports.default) { Object.defineProperty(exports.default, '__esModule', { value: true }); for (var key in exports) exports.default[key] = exports[key]; module.exports = exports.default; }
+/* CJS INTEROP */ if (exports.__esModule && exports.default) { try { Object.defineProperty(exports.default, '__esModule', { value: true }); for (var key in exports) { exports.default[key] = exports[key]; } } catch (_) {}; module.exports = exports.default; }
