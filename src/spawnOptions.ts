9394adb229133378aecdc2294b13c055cbbbe098
Kevin Malakoff <kmalakoff@gmail.com>
2025-05-19 21:26:44 -0700

    Removed non-standard npm environment variables

--- src/spawnOptions.ts
+++ src/spawnOptions.ts
@@ -14,12 +14,10 @@ import type { ProcessEnv, SpawnOptions } from './types.js';
 export default function spawnOptions(installPath: string, options: object = {}): SpawnOptions {
   const PATH_KEY = pathKey();
   const processEnv = process.env;
+  const bin = isWindows ? installPath : path.join(installPath, 'bin');
   const env = {} as ProcessEnv;
-  env.npm_config_binroot = isWindows ? installPath : path.join(installPath, 'bin');
-  env.npm_config_root = isWindows ? installPath : path.join(installPath, 'lib');
-  env.npm_config_man = isWindows ? installPath : path.join(installPath, 'man');
+  env.npm_node_execpath = path.join(bin, NODE);
   env.npm_config_prefix = installPath;
-  env.npm_node_execpath = path.join(env.npm_config_binroot, NODE);
 
   // copy the environment not for npm and skip case-insesitive additional paths
   for (const key in processEnv) {
@@ -34,6 +32,6 @@ export default function spawnOptions(installPath: string, options: object = {}):
   if (env.NODE_EXE !== undefined) env.NODE_EXE = env.npm_node_execpath;
 
   // put the path to node and npm at the front and remove nvs
-  env[PATH_KEY] = prepend(env[PATH_KEY] || '', env.npm_config_binroot) as string;
+  env[PATH_KEY] = prepend(env[PATH_KEY] || '', bin) as string;
   return { ...options, cwd: process.cwd(), env } as SpawnOptions;
 }
--- src/types.ts
+++ src/types.ts
@@ -2,9 +2,6 @@ import type cp from 'child_process';
 
 export type SpawnOptions = cp.SpawnOptions;
 export interface ProcessEnv extends NodeJS.ProcessEnv {
-  npm_config_binroot: string;
-  npm_config_root: string;
-  npm_config_man: string;
   npm_config_prefix: string;
   npm_node_execpath: string;
   NODE: string;
