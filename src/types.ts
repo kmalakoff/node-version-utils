b7d0759312a920b55fa5efb6efe8f1d9557b396f
Kevin Malakoff <kmalakoff@gmail.com>
2025-06-21 14:25:04 -0700

    Comprehensive types

--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,5 @@
-import type cp from 'child_process';
+export type { SpawnOptions } from 'cross-spawn-cb';
 
-export type SpawnOptions = cp.SpawnOptions;
 export interface ProcessEnv extends NodeJS.ProcessEnv {
   npm_config_prefix: string;
   npm_node_execpath: string;
