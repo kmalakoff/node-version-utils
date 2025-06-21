209da358d857a8f8a854a5dd438a61ca571c5ff1
Kevin Malakoff <kmalakoff@gmail.com>
2025-06-21 13:57:47 -0700

    Comprehensive types

--- src/lib/startsWithFn.ts
+++ src/lib/startsWithFn.ts
@@ -1,7 +1,9 @@
-export default function startsWithFn(string) {
+export type Compare = (key: string) => boolean;
+
+export default function startsCaseInsensitiveFn(string: string): Compare {
   const lower = string.toLowerCase();
   const upper = string.toUpperCase();
-  return function startsCaseInsensitive(key) {
+  return function startsCaseInsensitive(key: string): boolean {
     if (key.length < string.length) return false;
     for (let i = 0; i < string.length; i++) {
       if (key[i] !== lower[i] && key[i] !== upper[i]) return false;
