615433b88745ccd67baaed07f0ac67ade8f35b51
Kevin Malakoff <kmalakoff@gmail.com>
2025-10-20 21:30:08 -0700

    Upgrade modules

--- test/exports/import.test.ts
+++ test/exports/import.test.ts
@@ -1,8 +1,6 @@
 import assert from 'assert';
 
-// @ts-ignore
 import * as utils from 'node-version-utils';
-// @ts-ignore
 import { spawnOptions } from 'node-version-utils';
 
 describe('exports .ts', () => {
